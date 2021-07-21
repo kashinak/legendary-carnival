5f46207f4dc76823cf461aa1c99c65006b687dd1
kashinak <kashinakfx@gmail.com>
2021-07-21 04:32:27 +0000

    add memory event listener to game div so that the whole game baord becomes clickable making it more efficient to not hav eto write a game listener for 20 game cards

--- assets/script.js
+++ assets/script.js
@@ -50,7 +50,73 @@ flipped and once they are both flipped they will flip back */
 var cardChk = 0;
 //keep track of the score
 var correct = 0;
+
+/*We neded to retrieve game div area. we are going to use 
+this entire game element to see if someone clicks on it*/
+var memory = document.getElementById("game");
+/* we also need to retrieve the timer, score and new game button*/
+var timer = document.getElementById("timer");
+var scoreEl = document.getElementById("score");
+var newGame = document.getElementById("new");
+/*Next we need to retrieve the result, opacityD div*/
+var result = document.getElementById('result');
+var opacityD = document.getElementById('opacityD');
+/*since h1Res and pREs both have to be changed then we need to retrieve those as well*/
+var h1Res = document.getElementById('h1Res');
+var pRes = document.getElementById('pRes');
+/* we need to create a status variable and it's going to be assigned to 0 and it will 
+keep track of the uniqueness of the random number. so when it is 0 that means a unique
+number has not been generated yet. so every time a random number is generated in the 'rand' 
+value it needs to be unique that way there will not be any repitition, it will make sure 
+that the image that is retrieved is unique*/
+var status = 0;
+/* another array countDown will keep track of the count down timer. we are going to 
+create a set value of JavaScript predefined function that is going to set the timer
+and we are going to keep track of that timer with the countDown values; you'll see 
+how that works when we get through it*/
+var countDown;
+/* We are alos going to create a varibale that is changeable.  game begins at 60 seconds
+and counts down from there */
+var secsInput = 60;
+/* once I create a static value of secsInput = 60, that can't be changed, I can 
+apply the secsInput value for the countdown from 60sec, 59sec, 58sec... */
+var seconds = secsInput;
+/* to keep track of if the game is over or not I will create a varibale gameOver 
+and give it a default value of 'false'. If the game is over I won;t be able
+click on any of the tiles; game is over; click on the new game button to start a 
+new game. gameOver will be 'true' when the timer runs out and the user has 
+scored a perfect 10  */
+var gameOver = false;
+
 //2. Make the flipping work
+
+/* need to add an event listener and find out what element was flipped. 
+Instead of adding an event listener to each of the 20 cards, I am going to add 
+an event listener to the 'game' div that no matter where in that area is clicked,
+the assigned function will run. use memory variable already retrieved, add event 
+listener, add "click" event and function with 'e'(event object)in function 
+parenthesis. 'function' is anonomous because it doesn't have a name, it 
+doesn't have a function call because it doesn't need one since when the click 
+event happens on the memory, the function will run*. 'e' can be any word you want
+but whatever is inside those parenthesis is called an 'event object' */
+memory.addEventListener("click", function(e) {
+    /*event object - information about the event;
+    for example what exact element was clicked,
+    id of that element, when the event happened. 
+    once we find the target element that was clicked on, 
+    we can find the parent of that element, 
+    parent of that parent's id. This is what the 'e' 
+    element is going to help us do. we are going to create a 
+    variable called 'el' which is going to contain the 
+    parent element of the target. so we are going to call the event object 'e'
+    and find the target that was clicked and then find the aprent 
+    element of that target element*/
+    var el = e.target.parentElement
+    /* let's first retrieve the id */
+    var numId = el.id;
+});
+
+
 /*3. Basic game - no randomization, no timer, just flipping, comparing 
 and alert box for result. so have the game working without all 
 of the fancy things.*/
